e927afdc86676ae02996518e335d4fe00ddb1b22
Ryan <Ryan2@MacBook-Air-2.local>
2021-11-24 21:47:44 +0000

    pain severe pain

---
+++ IdeaProjects/FirstDay/src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello RYAN");
+
+        System.out.println(2+3);
+
+        int x = 3;
+        System.out.println(x);
+
+        String y = ("PAIN");
+        System.out.println(y);
+ int myInt = 124;
+ System.out.println(myInt);
+
+ byte myByte = 120;
+ System.out.println(myByte + 1);
+
+
+ int myInt1 = 54;
+ double myDouble = myInt1;
+ System.out.println(myDouble);
+
+int a = 14;
+        System.out.println(a);
+char o = 'Y';
+System.out.println(o);
+
+String name = "Hello!";
+System.out.println(name);
+
+double myDouble2 = 2.5;
+System.out.println(myDouble2);
+
+int u = ++a;
+System.out.println(a + u);
+
+System.out.println(10*5);
+System.out.println(10/5);
+
+int i = 10;
+++i;
+System.out.println(i);
+
+String thanks = "Thank You,";
+String lame = " Miguel!";
+String order = "Your order number is #";
+int previousOrder = 715;
+System.out.println(thanks + lame);
+System.out.println(order + previousOrder);
+    }
+
+}
+
